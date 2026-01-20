64a9f8db84cd8b4351ae9eb76e09a6e87b6a284a
Qilmeg  Doudatcz <qilmegdoudatcz@C02DF9YQMD6M.tld>
2026-01-20 12:38:19 -0500

    client ID etc info updated

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL ME IN";
+const CLIENT_ID = "591627307861-idhri9haqvo1g4vqfhodahckgp3m6q6t.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
